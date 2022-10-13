b99dd870c7225ad1172ee9fdf2835d6a5a87de1e
matias salvatore <[EMAIL]>
2022-10-13 12:22:19 +0200

    api conectada y funcionando

--- main.js
+++ main.js
@@ -7,186 +7,23 @@ const notaElement = document.querySelector(".nota");
 
 let currentQuestionIndex;
 let nota = 0;
-let results = [
-  {
-    "category": "General Knowledge",
-    "type": "boolean",
-    "difficulty": "medium",
-    "question": "Furby was released in 1998.",
-    "correct_answer": "True",
-    "incorrect_answers": [
-      "False"
-    ]
-  },
-  {
-    "category": "Entertainment: Video Games",
-    "type": "multiple",
-    "difficulty": "hard",
-    "question": "In the original &quot;Super Mario Bros.&quot;, what is the acceleration of Mario if he was in free fall?",
-    "correct_answer": "91.28 m/s^2",
-    "incorrect_answers": [
-      "110  m/s^2",
-      "9.42  m/s^2",
-      "4.4  m/s^2"
-    ]
-  },
-  {
-    "category": "Mythology",
-    "type": "boolean",
-    "difficulty": "easy",
-    "question": "According to Greek Mythology, Zeus can control lightning.",
-    "correct_answer": "True",
-    "incorrect_answers": [
-      "False"
-    ]
-  },
-  {
-    "category": "Entertainment: Books",
-    "type": "multiple",
-    "difficulty": "medium",
-    "question": "Which American author was also a budding travel writer and wrote of his adventures with his dog Charley?",
-    "correct_answer": "John Steinbeck",
-    "incorrect_answers": [
-      "F. Scott Fitzgerald",
-      "Ernest Hemingway",
-      "William Faulkner"
-    ]
-  },
-  {
-    "category": "Entertainment: Music",
-    "type": "multiple",
-    "difficulty": "easy",
-    "question": "Which Nirvana album had a naked baby on the cover?",
-    "correct_answer": "Nevermind",
-    "incorrect_answers": [
-      "Bleach",
-      "In Utero",
-      "Incesticide"
-    ]
-  },
-  {
-    "category": "Entertainment: Music",
-    "type": "multiple",
-    "difficulty": "medium",
-    "question": "African-American performer Sammy Davis Jr. was known for losing which part of his body in a car accident?",
-    "correct_answer": "Left Eye",
-    "incorrect_answers": [
-      "Right Ear",
-      "Right Middle Finger",
-      "Nose"
-    ]
-  },
-  {
-    "category": "Animals",
-    "type": "boolean",
-    "difficulty": "medium",
-    "question": "Finnish Lapphund dogs were used for herding reindeer.",
-    "correct_answer": "True",
-    "incorrect_answers": [
-      "False"
-    ]
-  },
-  {
-    "category": "Geography",
-    "type": "multiple",
-    "difficulty": "medium",
-    "question": "Which English county will you find the University of East Anglia?",
-    "correct_answer": "Norfolk",
-    "incorrect_answers": [
-      "Suffolk",
-      "Essex",
-      "Cambridgeshire"
-    ]
-  },
-  {
-    "category": "Science & Nature",
-    "type": "multiple",
-    "difficulty": "medium",
-    "question": "Which of these choices is not one of the phases of mitosis?",
-    "correct_answer": "Diplophase",
-    "incorrect_answers": [
-      "Metaphase",
-      "Anaphase",
-      "Telophase"
-    ]
-  },
-  {
-    "category": "History",
-    "type": "multiple",
-    "difficulty": "medium",
-    "question": "The Panama Canal was finished under the administration of which U.S. president?",
-    "correct_answer": "Woodrow Wilson",
-    "incorrect_answers": [
-      "Franklin Delano Roosevelt",
-      "Herbert Hoover",
-      "Theodore Roosevelt"
-    ]
-  }
-];
 
+let questions = [];
 
-// Object.values(results).forEach((item) => console.log(item.correct_answer + item.incorrect_answers))
-
-for (let i = 0; i < results.length; i++) {
-  let questionCyI = results [i];
-  let question = [{}]
-
-  console.log([questionCyI.correct_answer] + [questionCyI.incorrect_answers]);
+function converData() {
+  axios
+    .get("https://opentdb.com/api.php?amount=10&category=21")
+    .then((res) => {
+      console.log(res.data.results);
+      questions = res.data.results;
+    })
+    .catch((err) => console.error(err));
 }
 
-
-
-
-// // empezamos a generar los datos para construir nuesro objeto
-let incorrect = [];
-incorrect = results.map(function (elem) {
-  let returnIncorrect = {incorrect_answers: `text: ${elem.incorrect_answers}, correct: ${"false"}`};
-  return returnIncorrect;
-});
-console.log(incorrect);
-
-//aislamos correctas
-let corrects = [];
-corrects = results.map(function (elem) {
-  let returnCorrects = {correct_answer: `text: ${elem.correct_answer}, correct: ${"true"}`};
-  return returnCorrects;
-});
-console.log(corrects);
-
-let questionGlobal = [];
-questionGlobal = results.map(function (elem) {
-  let returnQuestion = {question: elem.question};
-  return returnQuestion;
-});
-console.log(questionGlobal);
-
-
-const answers = {incorrect,corrects}
-console.log(answers);
-
-// creadno el objeto desde los objetos de la api
-function crearObjetoDesdeArreglo(datos) {
-  let questions  = {};
-
-    for (const e of datos) {
-      questions[e[0]] = e[1];
-    }
-    return questions;
-}
-
-let propiedadesValores = [['question', questionGlobal],['answers', answers]];
-console.log(crearObjetoDesdeArreglo(propiedadesValores))
-
-//nuestro objeto
-const question = {
-  question: ['question', questionGlobal],
-  answers : ['answers', answers]
-}
-
-
-// empieza el codigo de sofi
+converData();
 
 function startGame() {
+  converData();
   startButton.classList.add("hide");
   currentQuestionIndex = 0;
   questionContainerElement.classList.remove("hide");
@@ -195,7 +32,13 @@ function startGame() {
 
 function showQuestion(question) {
   questionElement.innerText = question.question;
-  question.answers.forEach((answer) => {
+  let answers = [];
+  question.incorrect_answers.forEach((incorrectAnswer) =>
+    answers.push({ text: incorrectAnswer, correct: false })
+  );
+  answers.push({ text: question.correct_answer, correct: true });
+  console.log(answers);
+    answers.forEach((answer) => {
     const button = document.createElement("button");
     button.innerText = answer.text;
 
@@ -214,8 +57,8 @@ function showQuestion(question) {
         if (nota != 0) {
           nota = nota - 0.5;
           notaElement.innerHTML = "Tu puntuación: " + nota;
-        }else{
-            notaElement.innerHTML = "Tu puntuación: " + nota;
+        } else {
+          notaElement.innerHTML = "Tu puntuación: " + nota;
         }
       }
 
@@ -227,7 +70,8 @@ function showQuestion(question) {
 
 function setNextQuestion() {
   resetState();
-  showQuestion(results[currentQuestionIndex]);
+  console.log(questions);
+  showQuestion(questions[currentQuestionIndex]);
 }
 
 function setStatusClass(element, correct) {
